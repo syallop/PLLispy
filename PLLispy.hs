b2d45c0327ae80361a4bab2d7a88e17080f0f101
Samuel A. Yallop <syallop@gmail.com>
2018-03-30 20:48:53 +0100

    Steal lispy syntax from PL

---
+++ PLLispy.hs
@@ -0,0 +1,16 @@
+{-|
+Module      : PLLispy
+Copyright   : (c) Samuel A. Yallop, 2016
+Maintainer  : syallop@gmail.com
+Stability   : experimental
+
+A Grammar for PL with a lisp-like syntax.
+-}
+module PLLispy (module X) where
+
+import PLLispy.Case      as X
+import PLLispy.Expr      as X
+import PLLispy.Kind      as X
+import PLLispy.MatchArg  as X
+import PLLispy.Type      as X
+
---
+++ PLLispy/Kind.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-|
+Module      : PLLispy.Kind
+Copyright   : (c) Samuel A. Yallop, 2016
+Maintainer  : syallop@gmail.com
+Stability   : experimental
+
+A Grammar for PL.Kind with a lisp-like syntax.
+-}
+module PLLispy.Kind where
+
+import Control.Applicative
+
+import PL.Kind
+
+import PLLispy.KindIso
+
+import PLGrammar
+import PLGrammar.Iso
+
+kindAbs :: Grammar Kind
+kindAbs = kind
+
+kind :: Grammar Kind
+kind =  kind'
+    \|/ (try $ betweenParens kind')
+  where
+    kind' :: Grammar Kind
+    kind' = simpleKind \|/ arrowKind
+
+simpleKind :: Grammar Kind
+simpleKind = textIs "KIND" */ GPure Kind
+
+arrowKind :: Grammar Kind
+arrowKind = arrow */ (kindArrowIso \$/ kind \*/ (spaceRequired */ kind))
+
